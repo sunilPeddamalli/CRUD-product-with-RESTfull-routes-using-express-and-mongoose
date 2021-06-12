0ae633722d61592a673094dc16c126f2643a00f7
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-06-12 16:59:08 -0700

    created new ejs file to GET diplay form and create new product which is added to DB using POST

--- index.js
+++ index.js
@@ -16,16 +16,30 @@ mongoose.connect('mongodb://localhost/firstDB', { useNewUrlParser: true, useUnif
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
     res.render('products/index', { products })
 });
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save()
+    res.redirect(`/products/${newProduct._id}`)
+})
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', { product });
 })
+
 
 app.listen(3000, () => {
     console.log('Listening to Port 3000');
